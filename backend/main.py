bb859c3dfbcc4193a373e70eca249e192af0e1fa
Piyush Raj <sciencetechoindia@gmail.com>
2026-05-09 02:53:41 +0530

    fix: update CORS settings to restrict allowed origins for security

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://resume-analyzer-theta-seven.vercel.app"
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
